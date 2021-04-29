8297e33608fafc50e74057b2286b1f8b868583eb
Lucas <lucasoprezende@gmail.com>
2021-04-29 16:50:55 -0300

    adicao classes Compra, CompraTeste e Item

---
+++ src/oo/composicao/Compra.java
@@ -0,0 +1,29 @@
+package oo.composicao;
+
+import java.util.ArrayList;
+
+public class Compra {
+	
+	//Relação 1 para n: 1 compra tem muitos itens;
+	String cliente;
+	ArrayList<Item> itens = new ArrayList<Item>();
+	
+	void adicionarItem(Item item) {
+		itens.add(item);
+		item.compra = this;
+	}
+	
+	//metodo para saber o valor total da compra
+	double obterValorTotal () {
+		double total = 0;
+		for(Item item: itens) {
+			total += item.quantidade * item.preco;
+		}
+		return total;
+		
+	}
+	
+	
+	
+
+}
